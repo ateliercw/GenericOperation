f638af629222250ec5ec705aa210600b2521bb8d
Michael Skiba <mike.skiba@raizlabs.com>
2016-07-29 07:50:48 -0400

    Initial Commit

---
+++ GenericOperation/GenericOperation.h
@@ -0,0 +1,19 @@
+//
+//  GenericOperation.h
+//  GenericOperation
+//
+//  Created by Michael Skiba on 7/29/16.
+//  Copyright © 2016 Atelier Clockwork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericOperation.
+FOUNDATION_EXPORT double GenericOperationVersionNumber;
+
+//! Project version string for GenericOperation.
+FOUNDATION_EXPORT const unsigned char GenericOperationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericOperation/PublicHeader.h>
+
+
